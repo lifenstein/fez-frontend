3b2243efc9e27a5475e6827a7bcabb0e78ccc4b6
uqmmoise <m.moises@library.uq.edu.au>
2021-11-02 11:34:25 +1000

    Revert "Squashed commit of the following:"
    This reverts commit af14e1efb2591ddc52f09c5979abf24665f6b029.

--- src/locale/publicationForm.js
+++ src/locale/publicationForm.js
@@ -1496,7 +1496,7 @@ export default {
         pageTitle: 'Add data collection',
         information: {
             agreement: {
-                title: 'Deposit agreement',
+                title: 'Deposit agree[TYPO]',
                 text: (
                     <div>
                         <p>
