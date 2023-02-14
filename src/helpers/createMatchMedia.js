29db2d41baac152661d73c3bb3d4fd1015239e33
Lee Sibbald <lsibbald100+git@gmail.com>
2023-02-14 18:48:04 +1000

    Missing Test coverage

--- src/helpers/createMatchMedia.js
+++ src/helpers/createMatchMedia.js
@@ -3,7 +3,9 @@ import mediaQuery from 'css-mediaquery';
 export default function createMatchMedia(width) {
     return query => ({
         matches: mediaQuery.match(query, { width }),
+        /* istanbul ignore next */
         addListener: () => {},
+        /* istanbul ignore next */
         removeListener: () => {},
     });
 }
