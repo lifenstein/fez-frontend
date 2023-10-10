efd8141caac8d67d9c46d84effe16dad4c136c00
Steve Lancaster <depictedminds@outlook.com>
2023-10-10 12:04:27 +1000

    [185507439] Snaps updated with new button testID
    Snaps and cleanups based on current stage configuration

--- src/modules/SharedComponents/PublicationForm/components/Forms/CreativeWorkForm.js
+++ src/modules/SharedComponents/PublicationForm/components/Forms/CreativeWorkForm.js
@@ -164,7 +164,6 @@ export default class CreativeWorkForm extends Component {
                         />
                     </StandardCard>
                 </Grid>
-                {console.log('This.props.ntro', this.props.isNtro)}
                 {this.props.isNtro && (
                     <NtroFields
                         canEdit
