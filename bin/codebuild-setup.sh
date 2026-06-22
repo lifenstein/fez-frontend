fc45bac7691ddfaa3b1b5021a936cfc929c4114e
John Zeng <john.zeng@uq.edu.au>
2026-06-22 10:14:34 +1000

    [AD-936] install pnpm

--- bin/codebuild-setup.sh
+++ bin/codebuild-setup.sh
@@ -3,12 +3,11 @@
 # exit if command returns error (non-zero status)
 set -e
 
+corepack enable
+corepack use pnpm@11.7.0
+
 printf "Node "; node -v;
 printf "(Codeship default) pnpm v"; pnpm -v
-
-printf "\n\n--- GET LATEST VERSION OF pnpm 11.12.1 ---\n"
-echo "$ pnpm install -g pnpm@11.12.1"
-pnpm install -g pnpm@11.12.1
 
 printf "\nNow running pnpm v"; pnpm -v
 
@@ -17,5 +16,4 @@ printf "\n$ pnpm cache clear\n"
 pnpm cache clear -f
 
 printf "\n\n--- INSTALL DEPENDENCIES ---\n"
-echo "$ pnpm ci"
-pnpm ci
+pnpm install
